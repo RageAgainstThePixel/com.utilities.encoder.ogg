0503e3d82460bc329d2a51b38f86891265572251
Stephen Hodgson <hodgson.designs@gmail.com>
2023-02-24 04:03:42 -0500

    com.utilities.encoder.ogg 2.0.0 (#3)
    - Updated OggVorbisEncoder plugin to 1.2.2
    - Added `AudioClipExtensions` for converting `AudioClips` directly to .ogg
    - Updated demo script

---
+++ Runtime/Constants.cs
@@ -0,0 +1,11 @@
+namespace Utilities.Encoding.OggVorbis
+{
+    public static class Constants
+    {
+        public const int Channels = 2;
+
+        public const int Frequency = 44100;
+
+        public const float RescaleFactor = 32768f;
+    }
+}
