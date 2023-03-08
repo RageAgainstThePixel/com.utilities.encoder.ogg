943374556244d3fcae80318fb91ce37b68c15848
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-07 22:39:58 -0500

    com.utilities.encoder.ogg 3.0.0 (#5)
    - refactor with new audio utilities package dependency
    - renamed `EncodeAudioClipToOggVorbis` -> `EncodeToOggVorbis`
    - moved `RecordingManager` to audio utilities package
    - added `OggRecorderBehaviour`

--- com.utilities.encoder.ogg/Packages/com.com.utilities.encoder.ogg/Runtime/AudioClipExtensions.cs
+++ com.utilities.encoder.ogg/Packages/com.com.utilities.encoder.ogg/Runtime/AudioClipExtensions.cs
@@ -1,5 +1,10 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Assertions;
+using Utilities.Async;
+using Utilities.Audio;
 
 namespace Utilities.Encoding.OggVorbis
 {
@@ -11,56 +16,27 @@ namespace Utilities.Encoding.OggVorbis
         /// <param name="audioClip">The <see cref="AudioClip"/> to encode.</param>
         /// <param name="trim">Optional, trim silence at beginning and end of clip.</param>
         /// <returns><see cref="AudioClip"/> encoded to OggVorbis as byte array.</returns>
-        public static byte[] EncodeAudioClipToOggVorbis(this AudioClip audioClip, bool trim = false)
+        public static byte[] EncodeToOggVorbis(this AudioClip audioClip, bool trim = false)
         {
-            var samples = new float[audioClip.samples * audioClip.channels];
-            var sampleCount = samples.Length;
-            audioClip.GetData(samples, 0);
+            var pcmData = audioClip.EncodeToPCM(PCMFormatSize.SixteenBit, trim);
+            var rawSamples = OggEncoder.ConvertPcmData(audioClip.frequency, audioClip.channels, pcmData, audioClip.frequency, audioClip.channels);
+            var rawOggBytes = OggEncoder.ConvertToBytes(rawSamples, audioClip.frequency, audioClip.channels);
+            return rawOggBytes;
+        }
 
-            // trim data
-            var start = 0;
-            var end = sampleCount;
-
-            if (trim)
-            {
-                for (var i = 0; i < sampleCount; i++)
-                {
-                    if (samples[i] * Constants.RescaleFactor == 0)
-                    {
-                        continue;
-                    }
-
-                    start = i;
-                    break;
-                }
-
-                for (var i = sampleCount - 1; i >= 0; i--)
-                {
-                    if (samples[i] * Constants.RescaleFactor == 0)
-                    {
-                        continue;
-                    }
-
-                    end = i + 1;
-                    break;
-                }
-            }
-
-            var trimmedLength = end - start;
-            Assert.IsTrue(trimmedLength > 0);
-            var sampleIndex = 0;
-            var pcmData = new byte[trimmedLength * sizeof(float)];
-
-            // convert and write data
-            for (var i = start; i < end; i++)
-            {
-                var sample = (short)(samples[i] * Constants.RescaleFactor);
-                pcmData[sampleIndex++] = (byte)(sample >> 0);
-                pcmData[sampleIndex++] = (byte)(sample >> 8);
-            }
-
-            var rawSamples = Encoder.ConvertPcmData(audioClip.frequency, audioClip.channels, pcmData, audioClip.frequency, audioClip.channels);
-            var rawOggBytes = Encoder.ConvertToBytes(rawSamples, audioClip.frequency, audioClip.channels);
+        /// <summary>
+        /// Encodes the <see cref="AudioClip"/> to OggVorbis
+        /// </summary>
+        /// <param name="audioClip">The <see cref="AudioClip"/> to encode.</param>
+        /// <param name="trim">Optional, trim silence at beginning and end of clip.</param>
+        /// <param name="cancellationToken">Optional, <see cref="CancellationToken"/>.</param>
+        /// <returns><see cref="AudioClip"/> encoded to OggVorbis as byte array.</returns>
+        public static async Task<byte[]> EncodeToOggVorbisAsync(this AudioClip audioClip, bool trim = false, CancellationToken cancellationToken = default)
+        {
+            await Awaiters.UnityMainThread;
+            var pcmData = audioClip.EncodeToPCM(PCMFormatSize.SixteenBit, trim);
+            var rawSamples = OggEncoder.ConvertPcmData(audioClip.frequency, audioClip.channels, pcmData, audioClip.frequency, audioClip.channels);
+            var rawOggBytes = await OggEncoder.ConvertToBytesAsync(rawSamples, audioClip.frequency, audioClip.channels, cancellationToken: cancellationToken).ConfigureAwait(false);
             return rawOggBytes;
         }
     }
---
+++ com.utilities.encoder.ogg/Packages/com.com.utilities.encoder.ogg/Runtime/OggExtensions.cs
@@ -0,0 +1,30 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using OggVorbisEncoder;
+
+namespace Utilities.Encoding.OggVorbis
+{
+    internal static class OggExtensions
+    {
+        public static void FlushPages(this OggStream oggStream, Stream output, bool force)
+        {
+            while (oggStream.PageOut(out var page, force))
+            {
+                output.Write(page.Header, 0, page.Header.Length);
+                output.Write(page.Body, 0, page.Body.Length);
+            }
+        }
+
+        public static async Task FlushPagesAsync(this OggStream oggStream, Stream output, bool force, CancellationToken cancellationToken = default)
+        {
+            while (oggStream.PageOut(out var page, force))
+            {
+                await output.WriteAsync(page.Header, 0, page.Header.Length, cancellationToken);
+                await output.WriteAsync(page.Body, 0, page.Body.Length, cancellationToken);
+            }
+        }
+    }
+}
