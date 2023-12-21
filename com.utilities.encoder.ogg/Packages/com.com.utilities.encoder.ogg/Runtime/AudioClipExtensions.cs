bb9f61a0dcb867c3fa7f30ee42055c315a4d8974
Stephen Hodgson <hodgson.designs@gmail.com>
2023-12-21 14:54:15 -0500

    major refactor

--- com.utilities.encoder.ogg/Packages/com.com.utilities.encoder.ogg/Runtime/AudioClipExtensions.cs
+++ com.utilities.encoder.ogg/Packages/com.com.utilities.encoder.ogg/Runtime/AudioClipExtensions.cs
@@ -1,5 +1,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -10,32 +11,42 @@ namespace Utilities.Encoding.OggVorbis
 {
     public static class AudioClipExtensions
     {
-        /// <summary>
-        /// Encodes the <see cref="AudioClip"/> to OggVorbis
-        /// </summary>
-        /// <param name="audioClip">The <see cref="AudioClip"/> to encode.</param>
-        /// <param name="trim">Optional, trim silence at beginning and end of clip.</param>
-        /// <returns><see cref="AudioClip"/> encoded to OggVorbis as byte array.</returns>
-        public static byte[] EncodeToOggVorbis(this AudioClip audioClip, bool trim = false)
-        {
-            var pcmData = audioClip.EncodeToPCM(PCMFormatSize.SixteenBit, trim);
-            var samples = PCMEncoder.Decode(pcmData, PCMFormatSize.SixteenBit);
-            var rawOggBytes = OggEncoder.ConvertToBytes(samples, audioClip.frequency, audioClip.channels);
-            return rawOggBytes;
-        }
+        [Obsolete("Use new overload with bitDepth")]
+        public static byte[] EncodeToOggVorbis(this AudioClip audioClip, bool trim)
+            => EncodeToOggVorbis(audioClip, PCMFormatSize.SixteenBit, trim);
 
         /// <summary>
         /// Encodes the <see cref="AudioClip"/> to OggVorbis
         /// </summary>
         /// <param name="audioClip">The <see cref="AudioClip"/> to encode.</param>
+        /// <param name="bitDepth">Optional, Bit Depth to use for encoding. Defaults to <see cref="PCMFormatSize.SixteenBit"/>.</param>
+        /// <param name="trim">Optional, trim silence at beginning and end of clip.</param>
+        /// <returns><see cref="AudioClip"/> encoded to OggVorbis as byte array.</returns>
+        public static byte[] EncodeToOggVorbis(this AudioClip audioClip, PCMFormatSize bitDepth = PCMFormatSize.SixteenBit, bool trim = false)
+        {
+            var pcmData = audioClip.EncodeToPCM(bitDepth, trim);
+            var samples = PCMEncoder.Decode(pcmData, bitDepth);
+            var rawOggBytes = OggEncoder.ConvertToBytes(samples, audioClip.frequency, audioClip.channels);
+            return rawOggBytes;
+        }
+
+        [Obsolete("use new overload with bitDepth")]
+        public static async Task<byte[]> EncodeToOggVorbisAsync(this AudioClip audioClip, bool trim, CancellationToken cancellationToken = default)
+            => await EncodeToOggVorbisAsync(audioClip, PCMFormatSize.SixteenBit, trim, cancellationToken);
+
+        /// <summary>
+        /// Encodes the <see cref="AudioClip"/> to OggVorbis
+        /// </summary>
+        /// <param name="audioClip">The <see cref="AudioClip"/> to encode.</param>
+        /// <param name="bitDepth">Optional, Bit Depth to use for encoding. Defaults to <see cref="PCMFormatSize.SixteenBit"/>.</param>
         /// <param name="trim">Optional, trim silence at beginning and end of clip.</param>
         /// <param name="cancellationToken">Optional, <see cref="CancellationToken"/>.</param>
         /// <returns><see cref="AudioClip"/> encoded to OggVorbis as byte array.</returns>
-        public static async Task<byte[]> EncodeToOggVorbisAsync(this AudioClip audioClip, bool trim = false, CancellationToken cancellationToken = default)
+        public static async Task<byte[]> EncodeToOggVorbisAsync(this AudioClip audioClip, PCMFormatSize bitDepth = PCMFormatSize.SixteenBit, bool trim = false, CancellationToken cancellationToken = default)
         {
             await Awaiters.UnityMainThread;
-            var pcmData = audioClip.EncodeToPCM(PCMFormatSize.SixteenBit, trim);
-            var samples = PCMEncoder.Decode(pcmData, PCMFormatSize.SixteenBit);
+            var pcmData = audioClip.EncodeToPCM(bitDepth, trim);
+            var samples = PCMEncoder.Decode(pcmData, bitDepth);
             var rawOggBytes = await OggEncoder.ConvertToBytesAsync(samples, audioClip.frequency, audioClip.channels, cancellationToken: cancellationToken).ConfigureAwait(false);
             return rawOggBytes;
         }
---
+++ com.utilities.encoder.ogg/Packages/com.com.utilities.encoder.ogg/Tests/TestFixture_01_OGG_Encoding.cs
@@ -0,0 +1,52 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using NUnit.Framework;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Utilities.Audio;
+
+namespace Utilities.Encoding.OggVorbis.Tests
+{
+    public class TestFixture_01_OGG_Encoding
+    {
+        private const int Channels = 1;
+        private const int Frequency = 44100;
+
+        [Test]
+        public void Test_01_EncodeToOgg()
+        {
+            // Load 16-bit PCM sample
+            var raw16BitPcmPath = AssetDatabase.GUIDToAssetPath("252623e548216914b8abb0b584ceee72");
+            Assert.IsTrue(File.Exists(raw16BitPcmPath), "16-bit PCM sample file not found");
+
+            // Read PCM bytes
+            var pcm16BitBytes = File.ReadAllBytes(raw16BitPcmPath);
+            Assert.IsNotNull(pcm16BitBytes, "Failed to read 16-bit PCM bytes");
+            Assert.IsNotEmpty(pcm16BitBytes, "16-bit PCM bytes array is empty");
+
+            // Decode PCM bytes to samples
+            var samples = PCMEncoder.Decode(pcm16BitBytes);
+            Assert.IsNotNull(samples, "Failed to decode PCM bytes");
+            Assert.IsNotEmpty(samples, "Decoded samples array is empty");
+
+            // Create AudioClip
+            var audioClip = AudioClip.Create("16bit-sine", samples.Length, Channels, Frequency, false);
+            Assert.IsNotNull(audioClip, "Failed to create AudioClip");
+            audioClip.SetData(samples, 0);
+
+            // Encode to OGG
+            var encodedBytes = audioClip.EncodeToOggVorbis();
+
+            // Validate the result
+            Assert.IsNotNull(encodedBytes, "Failed to encode AudioClip to OGG");
+            Assert.IsNotEmpty(encodedBytes, "Encoded OGG bytes array is empty");
+
+            // Preliminary check OGG header
+            Assert.AreEqual('O', encodedBytes[0], "Incorrect OGG header");
+            Assert.AreEqual('g', encodedBytes[1], "Incorrect OGG header");
+            Assert.AreEqual('g', encodedBytes[2], "Incorrect OGG header");
+            Assert.AreEqual('S', encodedBytes[3], "Incorrect OGG header");
+        }
+    }
+}
